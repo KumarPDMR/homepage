a2883d63860e54424e699c24c6340cfbf7f5f321
KumarPDMR <kumararaja@pdmrindia.com>
2021-11-29 12:24:08 +0530

    updated home page

--- script.js
+++ script.js
@@ -168,4 +168,9 @@ window.onload = function() {
     });
 
 
+    const cursor = document.getElementById("cursor");
+    document.addEventListener("mousemove", e => {
+        cursor.setAttribute("style", `top: ${e.pageY}px; left: ${e.pageX}px;`)
+    })
+
 }
