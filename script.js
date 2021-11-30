fc57fed105156bf67ffff24203554725178154b7
KumarPDMR <kumararaja@pdmrindia.com>
2021-11-30 15:29:15 +0530

    added timing in animation

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var i = 1;
 var stopChangeImg = false;
-
+var stopBubble = false;
 
 function changeImg() {
     if (stopChangeImg) {
@@ -18,7 +18,7 @@ function changeImg() {
 
     let currentIndex = i;
 
-    setTimeout(function() {
+    setTimeout(function () {
         $(".image_slide" + currentIndex).css("animation", 'slideDown 2s');
     }, 200);
 
@@ -28,7 +28,9 @@ function changeImg() {
         i = i + 1;
     }
 
-    setTimeout(function() {
+    $(".image_slide1>div>img").attr("src", "COPS-LOGO.png");
+
+    setTimeout(function () {
         $(".image_slide" + i).css("animation", 'slide 2s');
         $(".image_slide" + i).css("z-index", '2');
     }, 0);
@@ -36,37 +38,37 @@ function changeImg() {
     setTimeout("changeImg()", 7000);
 }
 
-
 function loading_logo_anim() {
     $("#loading_logo").css("animation", 'none');
-    $("#loading_logo").attr("src","COPS-LOG_Anim_1.gif");
+    $("#loading_logo").attr("src", "COPS-LOG_Anim_1.gif");
 
-    setTimeout(()=> {
-    $("#loading_logo").css("animation", 'fade 3s forwards');
-    },6000);
+    setTimeout(() => {
+        $("#loading_logo").attr("src","COPS-LOGO.png");
+        $("#loading_logo").css("animation", 'fade 3s forwards');
+    }, 6000);
 
     setTimeout(() => {
         $("#loading_logo_container").css('background', 'transparent');
-        $("#loading_logo_container").removeClass('d-flex');
         $("#loading_logo_container").fadeOut();
+        $(".image_slide1>div>img").attr("src", "COPS-LOG_Anim_1.gif");
     }, 7000);
 
-    setTimeout(function() {
+    setTimeout(function () {
         $("#bubble").css("animation", 'none');
         $("#bubble").css("animation", 'createBubble 2s cubic-bezier(0.16,0.87,0.48,0.99) forwards');
     }, 1000);
 
     setTimeout("animateDiv('.bubble');", 2000);
 
-    setTimeout(function() {
+    setTimeout(function () {
         $("#main_container").attr("style", "");
     }, 2000);
 
-    setTimeout("changeImg()", 7000);
+    $(".image_slide1>div>img").attr("src", "COPS-LOGO.png");
+    setTimeout("changeImg()", 13000);
 }
 
-function makeNewPosition() {
-    // Get viewport dimensions (remove the dimension of the div)
+function makeNewPosition() { // Get viewport dimensions (remove the dimension of the div)
     var h = $(window).height() - 50;
     var w = $(window).width() - 50;
 
@@ -77,20 +79,34 @@ function makeNewPosition() {
 }
 
 function animateDiv(myclass) {
+    if (stopBubble) {
+        return;
+    }
     var newq = makeNewPosition();
-    $(myclass).animate({ top: newq[0], left: newq[1] }, 3000, function() {
+    $(myclass).animate({
+        top: newq[0],
+        left: newq[1]
+    }, 3000, function () {
         animateDiv(myclass);
     });
 };
 
 function onBubbleClicked() {
-    $("#bubble").css("animation", 'none');
-    $("#bubble").css("animation", 'pop 2s cubic-bezier(0.16,0.87,0.48,0.99) forwards');
+    $(".bubble").css("animation", 'none');
+    $(".bubble").css("animation", 'pop 2s cubic-bezier(0.16,0.87,0.48,0.99) forwards');
 
-    setTimeout(function() {
-        $("#bubble").css("animation", 'none');
-        $("#bubble").css("animation", 'createBubble 2s cubic-bezier(0.16,0.87,0.48,0.99) forwards');
-    }, 1000);
+    setTimeout(() => {
+        $(".bubble").css({top: 0, left: 0});
+        setTimeout(function () {
+            $(".bubble").css("animation", 'none');
+            $(".bubble").css("animation", 'createBubble 2s cubic-bezier(0.16,0.87,0.48,0.99) forwards');
+            stopBubble = false;
+            animateDiv(".bubble");
+        }, 4000);
+    }, 3000);
+
+    stopBubble = true;
+    
 }
 
 function onLoginButtonClicked() {
@@ -102,26 +118,18 @@ function onLoginButtonClicked() {
         $(".image_slide" + j).css("z-index", '');
     }
 
-    $(".image_slide1>div>img").attr("src","COPS-LOGO.png");
+    $(".image_slide1>div>img").attr("src", "COPS-LOGO.png");
 
     if ($(this).hasClass('material-button')) {
-        setTimeout(function() {
-            $(".overbox").css({
-                "overflow": "hidden"
-            })
+        setTimeout(function () {
+            $(".overbox").css({"overflow": "hidden"})
             // $(".box").addClass("back");
         }, 200);
 
 
+        $(this).addClass("active").animate({"width": "300px", "height": "500px", "border-radius": "10px"});
 
-
-        $(this).addClass("active").animate({
-            "width": "300px",
-            "height": "500px",
-            "border-radius":"10px"
-        });
-
-        setTimeout(function() {
+        setTimeout(function () {
             $(".box").fadeIn(300);
             $(".closeIcon").fadeIn(300);
             $(".material-button").fadeOut();
@@ -134,44 +142,45 @@ function onLoginCloseClicked() {
     $(".material-button").fadeIn();
     $(".box").fadeOut();
     $(".closeIcon").fadeOut();
-    $(".material-button").animate({
-        "width": "100px",
-        "height": "100px",
-        "border-radius":"50%"
-    });
+    $(".material-button").animate({"width": "100px", "height": "100px", "border-radius": "50%"});
     stopChangeImg = false;
     i = 1;
-    $(".image_slide1>div>img").attr("src","COPS-LOG_Anim_1.gif");
+    $(".image_slide1>div>img").attr("src", "COPS-LOG_Anim_1.gif");
     setTimeout("changeImg()", 6000);
 }
 
-
-window.onload = function() {
+window.onload = function () {
     var cursor = $('#cursor');
 
-    $(window).mousemove(function(e) {
+    $(window).mousemove(function (e) {
         cursor.css({
-           top: e.clientY - cursor.height() / 1,
-           left: e.clientX - cursor.width() / 1
+            top: e.clientY - cursor.height() / 1,
+            left: e.clientX - cursor.width() / 1
         });
     });
 
-    $(".material-button").mouseenter(function(e) {
-        cursor.addClass("login_cursor");
-        cursor.html("<div class='cursor_text'>LOGIN</div>")  
+    // $(".material-button").mouseenter(function (e) {
+    //     cursor.addClass("login_cursor");
+    //     cursor.html("<div class='cursor_text'>LOGIN</div>")
+    // });
+
+    $(".material-button").mouseout(function () {
+        cursor.removeClass("login_cursor");
+        cursor.html("")
     });
 
-    $(".material-button").mouseout(function() {
-            cursor.removeClass("login_cursor");
-            cursor.html("")  
-    });
+    $(".material-button").mousemove(function (e) {
+        console.log(e.clientX);
+        cursor.addClass("login_cursor");
+        cursor.html("<div class='cursor_text'>LOGIN</div>");
+    })
 
     $("#bubble").click(onBubbleClicked);
     $(".material-button").click(onLoginButtonClicked);
     $(".closeIcon").click(onLoginCloseClicked);
 
-    
-    $("#loading_logo").attr("src","COPS-LOGO.png");
+
+    $("#loading_logo").attr("src", "COPS-LOGO.png");
 
     setTimeout("loading_logo_anim()", 2000);
 
