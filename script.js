9d712f7b1447a9295b99a23d9ea8a1a4606de46c
KumarPDMR <kumararaja@pdmrindia.com>
2021-11-30 09:48:49 +0530

    update slide animation

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-var time = 5000;
 var i = 1;
 var stopChangeImg = false;
 
@@ -15,10 +14,10 @@ function changeImg() {
     }
 
     let currentIndex = i;
+        $(".image_slide" + currentIndex).css("z-index", '1');
 
     setTimeout(function() {
         $(".image_slide" + currentIndex).css("animation", 'slideDown 2s');
-        $(".image_slide" + currentIndex).css("z-index", '1');
     }, 200);
 
     if (i === 3) {
@@ -40,6 +39,7 @@ function changeImg() {
 function loading_logo_anim() {
     $("#loading_logo").css("animation", 'none');
     $("#loading_logo").css("animation", 'fade 3s forwards');
+
     setTimeout(() => {
         $("#loading_logo_container").css('background', 'transparent');
         $("#loading_logo_container").removeClass('d-flex');
@@ -56,9 +56,9 @@ function loading_logo_anim() {
     setTimeout(function() {
         $("#main_container").attr("style", "");
     }, 2000);
+
     setTimeout("changeImg()", 6000);
 }
-
 
 function makeNewPosition() {
     // Get viewport dimensions (remove the dimension of the div)
@@ -78,7 +78,6 @@ function animateDiv(myclass) {
     });
 };
 
-
 function onBubbleClicked() {
     $("#bubble").css("animation", 'none');
     $("#bubble").css("animation", 'pop 2s cubic-bezier(0.16,0.87,0.48,0.99) forwards');
@@ -89,42 +88,49 @@ function onBubbleClicked() {
     }, 1000);
 }
 
-window.onload = function() {
-    $("#bubble").click(onBubbleClicked);
-
-    setTimeout("loading_logo_anim()", 6150);
-
-    for (let j = 1; j <= 3; j++) {
-        $(".image_slide" + j).css("animation", 'none');
+function onLoginButtonClicked() {
+    stopChangeImg = true;
+    $(".image_slide" + 1).css("z-index", '1');
+    for (let j = 2; j <= 3; j++) {
+        $(".image_slide" + j).css("z-index", '0');
+        $(".image_slide" + j + ">div>img").css("display", "block");
     }
 
-    $(".material-button").click(function() {
-        stopChangeImg = true;
-        $(".image_slide" + 1).css("z-index", '2');
-        for (let j = 2; j <= 3; j++) {
-            $(".image_slide" + j).css("z-index", '0');
-            $(".image_slide" + j + ">div>img").css("display", "block");
-        }
+    if ($(this).hasClass('material-button')) {
+        setTimeout(function() {
+            $(".overbox").css({
+                "overflow": "hidden"
+            })
+            $(".box").addClass("back");
+        }, 200)
+        $(this).addClass("active").animate({
+            "width": "300px",
+            "height": "500px"
+        });
 
-        if ($(this).hasClass('material-button')) {
-            setTimeout(function() {
-                $(".overbox").css({
-                    "overflow": "hidden"
-                })
-                $(".box").addClass("back");
-            }, 200)
-            $(this).addClass("active").animate({
-                "width": "300px",
-                "height": "500px"
-            });
+        setTimeout(function() {
+            $(".box").fadeIn(300);
+            $(".material-button").fadeOut();
+        }, 700)
+    }
+}
 
-            setTimeout(function() {
-                $(".box").fadeIn(300);
-                $(".material-button").fadeOut();
-            }, 700)
-        }
-
+function onLoginCloseClicked() {
+    $(".material-button").fadeIn();
+    $(".box").fadeOut();
+    $(".material-button").animate({
+        "width": "140px",
+        "height": "140px"
     });
+    stopChangeImg = false;
+    i = 1;
+    setTimeout("changeImg()", 6000);
+}
+
+window.onload = function() {
+    $("#bubble").click(onBubbleClicked);
+    $(".material-button").click(onLoginButtonClicked);
+    $(".closeIcon").click(onLoginCloseClicked);
 
     const cursor = document.getElementById("cursor");
     document.addEventListener("mousemove", e => {
@@ -143,15 +149,10 @@ window.onload = function() {
         }
     });
 
-    $(".closeIcon").click(function() {
-        $(".material-button").fadeIn();
-        $(".box").fadeOut();
-        $(".material-button").animate({
-            "width": "140px",
-            "height": "140px"
-        });
-        stopChangeImg = false;
-        i = 1;
-        setTimeout("changeImg()", 6000);
-    });
+    setTimeout("loading_logo_anim()", 6150);
+
+    for (let j = 1; j <= 3; j++) {
+        $(".image_slide" + j).css("animation", 'none');
+    }
+
 }
