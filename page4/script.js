843cd6cdd95af2f60f5a69e11b56c65bc8d47585
KumarPDMR <kumararaja@pdmrindia.com>
2022-03-04 16:25:54 +0530

    updated

--- page4/script.js
+++ page4/script.js
@@ -1,7 +1,36 @@
-$(document).ready(() => {});
+$(document).ready(() => {
+  startAnim();
+});
 
-function reset_animation() {
-  var el = document.getElementById("card");
+function startAnim() {
+  $("#card>img").attr("src", "../assests/cops_logo.png");
+
+  setTimeout(() => {
+      $("#card>img").attr("src", "../assests/cops_logo_anim_b.gif");
+  }, 2000);
+
+  setTimeout(() => {
+      $("#card>img").attr("src", "../assests/cops_logo.png");
+
+  }, 7500);
+
+  setTimeout(() => {
+      $("#card").css("animation", "circleShrink 2s");
+  }, 8000);
+
+  setTimeout(() => {
+      $("#card").css("animation", "circleScale 2s");
+      reset_animation("outline")
+      $("#outline").css("animation", "outlineScale 2s");
+  }, 10000);
+
+  setTimeout(() => {
+      startAnim();
+  }, 12000);
+}
+
+function reset_animation(cls) {
+  var el = document.getElementById(cls);
   el.style.animation = "none";
   el.offsetHeight; /* trigger reflow */
   el.style.animation = null;
